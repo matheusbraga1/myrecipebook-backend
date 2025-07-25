6a20167a1e5d265fb7aae580fe0ec6a1c165556c
Matheus Braga <matheusgalaxytab@gmail.com>
2025-07-25 17:01:29 -0400

    feat: config automapper

---
+++ src/Backend/MyRecipeBook.Application/Services/Mappings/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.Services.Mappings;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToDomain();
+    }
+
+    private void RequestToDomain()
+    {
+        CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore());
+    }
+}
