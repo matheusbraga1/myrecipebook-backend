6a995fd4b404d7242e9bebb596b6fb9f384caa8d
Matheus Braga <matheusgalaxytab@gmail.com>
2025-08-05 17:10:23 -0400

    feat: creating password encrypter

---
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncrypter.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyRecipeBook.Application.Services.Cryptography;
+
+public class PasswordEncrypter
+{
+    public string EncryptPassword(string password)
+    {
+        var additionalKey = "ABC";
+        var newPassword = $"{password}{additionalKey}";
+
+        var bytes = Encoding.UTF8.GetBytes(newPassword);
+        var hashBytes = SHA512.HashData(bytes);
+
+        return ConvertToStringBytes(hashBytes);
+    }
+
+    private static string ConvertToStringBytes(byte[] bytes)
+    {
+        var sb = new StringBuilder();
+        foreach (var b in bytes)
+        {
+            sb.Append(b.ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+}
