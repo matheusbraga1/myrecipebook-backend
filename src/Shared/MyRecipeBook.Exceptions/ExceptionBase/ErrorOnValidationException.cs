c94b420bd21319e8acb90bb97a0b7dfa01fa3ef1
Matheus Braga <matheusgalaxytab@gmail.com>
2025-08-05 17:12:33 -0400

    feat: adjusting error on valid exception method

--- src/Shared/MyRecipeBook.Exceptions/ExceptionBase/ErrorOnValidationException.cs
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionBase/ErrorOnValidationException.cs
@@ -4,8 +4,5 @@ public class ErrorOnValidationException : MyRecipeBookException
 {
     public IList<string> ErrorMessages { get; set; }
 
-    public ErrorOnValidationException(IList<string> errorMessages)
-    {
-        ErrorMessages = errorMessages;
-    }
+    public ErrorOnValidationException(IList<string> errorMessages) => ErrorMessages = errorMessages;
 }
