da10160cb5576edb967136103cf545aa14b5fe6d
Matheus Braga <matheusgalaxytab@gmail.com>
2025-08-05 17:11:33 -0400

    feat: adding dependency injection extension method

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRecipeBook.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        
+    }
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infrastructure;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        AddRepositories(services);
+    }
+
+    private static void AddDbContext(IServiceCollection services)
+    {
+        services.AddDbContext<MyRecipeBookDbContext>(options =>
+        {
+            options.UseSqlServer("YourConnectionString");
+        });
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+    }
+}
