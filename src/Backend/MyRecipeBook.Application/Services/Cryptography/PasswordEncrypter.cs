302e41ee7d0ea004dfb85be19ed27de8f8984dd4
Matheus Braga <matheusgalaxytab@gmail.com>
2025-08-08 17:08:50 -0400

    feat: adjusting password encrypter method

--- src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncrypter.cs
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncrypter.cs
@@ -5,10 +5,13 @@ namespace MyRecipeBook.Application.Services.Cryptography;
 
 public class PasswordEncrypter
 {
+    private readonly string _additionalKey;
+
+    public PasswordEncrypter(string additionalKey) => _additionalKey = additionalKey;
+
     public string EncryptPassword(string password)
     {
-        var additionalKey = "ABC";
-        var newPassword = $"{password}{additionalKey}";
+        var newPassword = $"{password}{_additionalKey}";
 
         var bytes = Encoding.UTF8.GetBytes(newPassword);
         var hashBytes = SHA512.HashData(bytes);
