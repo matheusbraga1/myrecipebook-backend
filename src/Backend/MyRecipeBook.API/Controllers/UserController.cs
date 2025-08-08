a784656b2171c06a438fe9ae3be8462799cc4068
Matheus Braga <matheusgalaxytab@gmail.com>
2025-08-08 17:08:05 -0400

    feat: adjusting register method

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -11,11 +11,11 @@ public class UserController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
-    public IActionResult Register(RequestRegisterUserJson request)
+    public async Task<IActionResult> Register(
+        [FromServices] IRegisterUserUseCase useCase, 
+        [FromBody] RequestRegisterUserJson request)
     {
-        var useCase = new RegisterUserUseCase();
-
-        var result = useCase.Execute(request);
+        var result = await useCase.Execute(request);
 
         return Created(string.Empty, result);
     }
