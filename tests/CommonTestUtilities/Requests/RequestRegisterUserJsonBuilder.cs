c4ee9b576962be882213060268853c9fb7eacb1c
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-02 17:07:44 -0400

    feat: adding unity tests

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public static class RequestRegisterUserJsonBuilder
+{
+    public static RequestRegisterUserJson Build()
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+            .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+            .RuleFor(user => user.Password, (f, user) => f.Internet.Password());
+    }
+}
---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,51 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+
+namespace Validators.Test.User.Register;
+
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceMessageException.NAME_EMPTY);
+    }
+
+    [Fact]
+    public void Error_Email_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceMessageException.EMAIL_EMPTY);
+    }
+}
