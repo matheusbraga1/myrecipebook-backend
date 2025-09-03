96701cf7124b78e5a8e97ea7e3d0add1401d9dee
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-03 16:33:37 -0400

    feat: adding register user use case tests

---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,11 @@
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace UseCases.Test.User.Register;
+
+public class RegisterUserUseCaseTest
+{
+    public void Success()
+    {
+        var useCase = new RegisterUserUseCase();
+    }
+}
