468f57d1fbeccb73a919494b7a8b1af4af646844
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-03 16:32:29 -0400

    feat: adding parameter in Build method

--- tests/CommonTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
+++ tests/CommonTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -5,11 +5,11 @@ namespace CommonTestUtilities.Requests;
 
 public static class RequestRegisterUserJsonBuilder
 {
-    public static RequestRegisterUserJson Build()
+    public static RequestRegisterUserJson Build(int passwordLength = 10)
     {
         return new Faker<RequestRegisterUserJson>()
             .RuleFor(user => user.Name, (f) => f.Person.FirstName)
             .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
-            .RuleFor(user => user.Password, (f, user) => f.Internet.Password());
+            .RuleFor(user => user.Password, (f) => f.Internet.Password(passwordLength));
     }
 }
