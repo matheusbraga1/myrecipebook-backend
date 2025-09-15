fe74b6d64d6639f7caa26cfc0adea5bd444eea2f
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-15 16:24:43 -0400

    feat: adding password encrypter builder

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncrypterBuilder.cs
@@ -0,0 +1,8 @@
+﻿using MyRecipeBook.Application.Services.Cryptography;
+
+namespace CommonTestUtilities.Cryptography;
+
+public class PasswordEncrypterBuilder
+{
+    public static PasswordEncrypter Build() => new PasswordEncrypter("abc1234");
+}
