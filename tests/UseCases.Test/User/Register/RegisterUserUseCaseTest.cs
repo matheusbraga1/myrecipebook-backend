1137967ac075a032eb447d692aec8643e2dd2e18
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-15 16:26:42 -0400

    feat: adding success and create use case methods

--- tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -1,11 +1,35 @@
-﻿using MyRecipeBook.Application.UseCases.User.Register;
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using Shouldly;
 
 namespace UseCases.Test.User.Register;
 
 public class RegisterUserUseCaseTest
 {
-    public void Success()
+    [Fact]
+    public async Task Success()
     {
-        var useCase = new RegisterUserUseCase();
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(request.Name);
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        var mapper = MapperBuilder.Build();
+        var passwordEncrypter = PasswordEncrypterBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+        var readRepository = new UserReadOnlyRepositoryBuilder().Build();
+
+        return new RegisterUserUseCase(readRepository, writeRepository, unitOfWork, mapper, passwordEncrypter);
     }
 }
