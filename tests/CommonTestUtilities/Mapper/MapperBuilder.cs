cfd2d4d7151d76f5abb40f0a5b26f26f7e0a6ddf
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-15 16:25:01 -0400

    feat: adding mapper builder

---
+++ tests/CommonTestUtilities/Mapper/MapperBuilder.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MyRecipeBook.Application.Services.Mappings;
+
+namespace CommonTestUtilities.Mapper;
+
+public class MapperBuilder
+{
+    public static IMapper Build()
+    {
+        return new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new AutoMapping());
+        }).CreateMapper();
+    }
+}
