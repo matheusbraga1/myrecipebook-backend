bef0821c7a6de6707cb714867e095f3f979305e5
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-16 17:01:33 -0400

    feat: adding error_email_already_registered and error_name_empty methods for tests

--- tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -3,6 +3,8 @@ using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
 using Shouldly;
 
 namespace UseCases.Test.User.Register;
@@ -22,13 +24,47 @@ public class RegisterUserUseCaseTest
         result.Name.ShouldBe(request.Name);
     }
 
-    private RegisterUserUseCase CreateUseCase()
+    [Fact]
+    public async Task Error_Email_Already_Registered()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var useCase = CreateUseCase(request.Email);
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        (await Should.ThrowAsync<ErrorOnValidationException>(act))
+            .ErrorMessages.ShouldHaveSingleItem()
+            .ShouldBe(ResourceMessageException.EMAIL_ALREADY_REGISTERED);
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var useCase = CreateUseCase();
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        (await Should.ThrowAsync<ErrorOnValidationException>(act))
+            .ErrorMessages.ShouldHaveSingleItem()
+            .ShouldBe(ResourceMessageException.NAME_EMPTY);
+    }
+
+    private RegisterUserUseCase CreateUseCase(string? email = null)
     {
         var mapper = MapperBuilder.Build();
         var passwordEncrypter = PasswordEncrypterBuilder.Build();
         var unitOfWork = UnitOfWorkBuilder.Build();
         var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
-        var readRepository = new UserReadOnlyRepositoryBuilder().Build();
+        var readRepositoryBuilder = new UserReadOnlyRepositoryBuilder();
+
+        if (!string.IsNullOrEmpty(email))
+            readRepositoryBuilder.ExistActiveUserWithEmail(email);
+
+        var readRepository = readRepositoryBuilder.Build();
 
         return new RegisterUserUseCase(readRepository, writeRepository, unitOfWork, mapper, passwordEncrypter);
     }
