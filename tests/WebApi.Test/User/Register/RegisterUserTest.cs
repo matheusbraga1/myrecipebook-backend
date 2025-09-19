3196d0e94f17b0b22bb98cdbc604628f37db723a
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-19 17:17:45 -0400

    feat: adding integration tests

---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,34 @@
+﻿using CommonTestUtilities.Requests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.Test.User.Register;
+
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public RegisterUserTest(CustomWebApplicationFactory factory) => _httpClient = factory.CreateClient();
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var response = await _httpClient.PostAsJsonAsync("User", request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var name = responseData.RootElement.GetProperty("name").GetString();
+
+        name.ShouldNotBeNullOrWhiteSpace();
+        name.ShouldBe(request.Name);
+    }
+}
