e83c2682c75cf88e2a955fe115f630e2a318524d
Matheus Braga <matheusgalaxytab@gmail.com>
2025-09-22 17:22:14 -0400

    feat: adding tests for resource message exceptions

--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -1,9 +1,11 @@
 ﻿using CommonTestUtilities.Requests;
-using Microsoft.AspNetCore.Mvc.Testing;
+using System.Globalization;
+using MyRecipeBook.Exceptions;
 using Shouldly;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.User.Register;
 
@@ -30,5 +32,33 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 
         name.ShouldNotBeNullOrWhiteSpace();
         name.ShouldBe(request.Name);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Empty_Name(string culture)
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+            _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+
+        var response = await _httpClient.PostAsJsonAsync("User", request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessageException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+        errors.ShouldHaveSingleItem();
+        errors.ShouldContain(error => error.GetString()!.Equals(expectedMessage));
     }
 }
