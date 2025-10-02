888ae6a32e1e84d9eb56c3e0cd462ab30b82e026
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-02 17:02:15 -0400

    feat: registering login use case service

--- src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Application.Services.Cryptography;
 using MyRecipeBook.Application.Services.Mappings;
+using MyRecipeBook.Application.UseCases.Login.DoLogin;
 using MyRecipeBook.Application.UseCases.User.Register;
 
 namespace MyRecipeBook.Application;
@@ -27,6 +28,7 @@ public static class DependencyInjectionExtension
     public static void AddUseCases(IServiceCollection services)
     {
         services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        services.AddScoped<IDoLoginUseCase, DoLoginUseCase>();
     }
 
     public static void AddPasswordEncrypter(IServiceCollection services, IConfiguration configuration)
