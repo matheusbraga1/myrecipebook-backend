67a4c98fc5667484a5fc715ba2552d02969c9d72
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-07 17:19:43 -0400

    feat: adding jwt access token

---
+++ src/Backend/MyRecipeBook.Infrastructure/Security/Tokens/Access/Generator/JwtTokenGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MyRecipeBook.Domain.Security.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace MyRecipeBook.Infrastructure.Security.Tokens.Access.Generator;
+
+public class JwtTokenGenerator : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenGenerator(uint expirationTimeMinutes, string signingKey)
+    {
+        _expirationTimeMinutes = expirationTimeMinutes;
+        _signingKey = signingKey;
+    }
+
+    public string Generate(Guid userIdentifier)
+    {
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SecurityKey()
+    {
+        var bytes = Encoding.UTF8.GetBytes(_signingKey);
+
+        return new SymmetricSecurityKey(bytes);
+    }
+}
