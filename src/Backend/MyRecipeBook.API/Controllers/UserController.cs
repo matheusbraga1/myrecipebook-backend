7000584c8064633f184494bec6f6a784872df2d9
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-09 16:38:31 -0400

    feat: adding get user profile method

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyRecipeBook.API.Attributes;
+using MyRecipeBook.Application.UseCases.User.Profile;
 using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
 namespace MyRecipeBook.API.Controllers;
 
-[AuthenticatedUser]
 public class UserController : MyRecipeBookBaseController
 {
     [HttpPost]
@@ -18,5 +18,15 @@ public class UserController : MyRecipeBookBaseController
         var result = await useCase.Execute(request);
 
         return Created(string.Empty, result);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
+    [AuthenticatedUser]
+    public async Task<IActionResult> GetUserProfile([FromServices] IGetUserProfileUseCase useCase)
+    {
+        var result = await useCase.Execute();
+
+        return Ok(result);
     }
 }
