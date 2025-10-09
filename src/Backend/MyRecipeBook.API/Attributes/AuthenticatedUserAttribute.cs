121a5a134d110401c73c13f54f509eb1639e1aa9
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-09 14:06:12 -0400

    feat: adding attribute for authenticated user

---
+++ src/Backend/MyRecipeBook.API/Attributes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.API.Filters;
+
+namespace MyRecipeBook.API.Attributes;
+
+public class AuthenticatedUserAttribute : TypeFilterAttribute
+{
+    public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
+    {
+    }
+}
--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Attributes;
 using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
 namespace MyRecipeBook.API.Controllers;
 
-[ApiController]
+[AuthenticatedUser]
 public class UserController : MyRecipeBookBaseController
 {
     [HttpPost]
