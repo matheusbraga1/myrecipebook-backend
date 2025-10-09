096f4db89fbcfd4543b4e33729d37cc23e3705d4
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-09 14:06:34 -0400

    feat: adding attribute for authenticated user

---
+++ src/Backend/MyRecipeBook.API/Filters/AuthenticatedUserFilter.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Domain.Security.Tokens;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace MyRecipeBook.API.Filters;
+
+public class AuthenticatedUserFilter : IAsyncAuthorizationFilter
+{
+    private readonly IAccessTokenValidator _accessTokenValidator;
+    private readonly IUserReadOnlyRepository _repository;
+
+    public AuthenticatedUserFilter(IAccessTokenValidator accessTokenValidator, IUserReadOnlyRepository repository)
+    {
+        _accessTokenValidator = accessTokenValidator;
+        _repository = repository;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        try
+        {
+            var token = TokenOnRequest(context);
+
+            var userIdentifier = _accessTokenValidator.ValidateAndGetUserIdentifier(token);
+
+            var exist = await _repository.ExistsActiveUserWithIdentifier(userIdentifier);
+
+            if (!exist)
+            {
+                throw new MyRecipeBookException(ResourceMessageException.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE);
+            }
+        }
+        catch (SecurityTokenExpiredException)
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson("TokenIsExpired")
+            {
+                TokenExpired = true
+            });
+        }
+        catch (MyRecipeBookException ex)
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(ex.Message));
+        }
+        catch
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(ResourceMessageException.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE));
+        }
+    }
+
+    private static string TokenOnRequest(AuthorizationFilterContext context)
+    {
+        var authentication = context.HttpContext.Request.Headers.Authorization.ToString();
+
+        if (string.IsNullOrWhiteSpace(authentication))
+        {
+            throw new MyRecipeBookException(ResourceMessageException.NO_TOKEN);
+        }
+
+        return authentication["Bearer ".Length..].Trim();
+    }
+}
