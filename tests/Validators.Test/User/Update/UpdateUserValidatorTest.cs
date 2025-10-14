455fda0cd869c28bacaa497b3690fc2295d97880
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-14 17:02:14 -0400

    feat: adding tests for update user

---
+++ tests/Validators.Test/User/Update/UpdateUserValidatorTest.cs
@@ -0,0 +1,66 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Update;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+
+namespace Validators.Test.User.Update;
+
+public class UpdateUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceMessageException.NAME_EMPTY);
+    }
+
+    [Fact]
+    public void Error_Email_Empty()
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceMessageException.EMAIL_EMPTY);
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        var validator = new UpdateUserValidator();
+
+        var request = RequestUpdateUserJsonBuilder.Build();
+        request.Email = "matheus.com";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceMessageException.EMAIL_INVALID);
+    }
+}
