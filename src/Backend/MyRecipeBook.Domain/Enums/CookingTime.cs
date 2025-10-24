8e942e9336b5582308907002806e8572e6fd0f5a
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-24 17:11:11 -0400

    feat: add cookingTime/Difficulty/dishType enums

---
+++ src/Backend/MyRecipeBook.Domain/Enums/CookingTime.cs
@@ -0,0 +1,9 @@
+﻿namespace MyRecipeBook.Domain.Enums;
+
+public enum CookingTime
+{
+    Less_10_Minutes = 0,
+    Between_10_and_30_Minutes = 1,
+    Between_30_and_60_Minutes = 2,
+    Greather_60_Minutes = 3
+}
