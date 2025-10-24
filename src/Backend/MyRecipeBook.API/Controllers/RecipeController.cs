01645a2aae898b2c15aa9b93ae974362b151e5fb
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-24 17:09:24 -0400

    feat: add RecipeController for recipe endpoints

---
+++ src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Attributes;
+
+namespace MyRecipeBook.API.Controllers;
+
+[AuthenticatedUser]
+public class RecipeController : MyRecipeBookBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof())]
+    public async Task<IActionResult> Register()
+    {
+
+    }
+}
