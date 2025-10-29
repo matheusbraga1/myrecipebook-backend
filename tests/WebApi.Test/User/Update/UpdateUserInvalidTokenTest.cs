65c1bb4e0bb5f131fb3181f67438cdfe40792019
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-29 10:54:33 -0400

    feat: add update user with invalid token test

---
+++ tests/WebApi.Test/User/Update/UpdateUserInvalidTokenTest.cs
@@ -0,0 +1,44 @@
+﻿using CommonTestUtilities.Tokens;
+using MyRecipeBook.Communication.Requests;
+using Shouldly;
+using System.Net;
+
+namespace WebApi.Test.User.Update;
+public class UpdateUserInvalidTokenTest : MyRecipeBookClassFixture
+{
+    private readonly string METHOD = "user";
+
+    public UpdateUserInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory) { }
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var request = new RequestUpdateUserJson();
+
+        var response = await DoPut(METHOD, request, token: "tokenInvalid");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var request = new RequestUpdateUserJson();
+
+        var response = await DoPut(METHOD, request, token: string.Empty);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var token = JwtTokenGeneratorBuild.Build().Generate(Guid.NewGuid());
+
+        var request = new RequestUpdateUserJson();
+
+        var response = await DoPut(METHOD, request, token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
