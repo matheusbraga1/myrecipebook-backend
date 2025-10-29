855cd5ec2180322f93b168032a1ef632c2cb7340
Matheus Braga <matheusgalaxytab@gmail.com>
2025-10-29 10:54:00 -0400

    feat: adding string generator

---
+++ tests/CommonTestUtilities/Requests/RequestStringGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestStringGenerator
+{
+    public static string Paragraph(int minCharacters)
+    {
+        var faker = new Faker();
+
+        var longText = faker.Lorem.Paragraphs(count: 7);
+
+        while (longText.Length < minCharacters)
+        {
+            longText = $"{longText} {faker.Lorem.Paragraphs()}";
+        }
+
+        return longText;
+    }
+}
